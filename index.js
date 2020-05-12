59bf9798a703372dd41e8e85219f8562e77a8337
woodpeckerAnos <woodpeckeranos@foxmail.com>
2020-05-12 10:09:03 +0800

    确认当前爬虫的问题在于爬取的页面URL不正确，处理不准确，获取的不是一个完整的图片路径，而是当前域下的路径，没有对此进行处理；获取到的图片也没有对查询字符串进行处理，导致图片格式错误

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const fs = require('fs')
 const config = require('./config')
 const analyze = require('./analyze')
 
+const imgArr = [
+    'https://cn.bing.com/th?id=OHR.SeagullsChat_ZH-CN8973709588_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4&r=0',
+    'https://cn.bing.com/sa/simg/hpc27_2x.png',
+    'https://cn.bing.com/th?id=OHR.OldPatriarchTree_ZH-CN8818146190_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4',
+    'https://cn.bing.com/th?id=OHR.BarnOwlMigration_ZH-CN8579914070_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4',
+]
+
 function start() {
     request(config.url, (err, res, body) => {
         if (!err && res) {
@@ -15,6 +22,8 @@ function start() {
 }
 
 function download(imgUrl, index) {
+    imgUrl=imgArr[index]
+    if (!imgUrl) return
     let ext = imgUrl.split('.').pop()
     let imgFilePath = path.join(config.imgDir, `${index}.${ext}`)
     request(imgUrl).pipe(fs.createWriteStream(imgFilePath, { 'encoding': 'utf8' }))
